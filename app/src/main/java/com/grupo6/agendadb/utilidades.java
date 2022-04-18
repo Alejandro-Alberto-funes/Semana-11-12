5648313b2b7664565b0a80803bfa15c3c9f0bf74
GEOVANNI ALEXANDER GUERRERO MUNGUIA <USIS015221@ugb.edu.sv>
2022-04-17 20:51:18 -0600

    Initial commit

---
+++ app/src/main/java/com/grupo6/agendadb/utilidades.java
@@ -0,0 +1,6 @@
+package com.grupo6.agendadb;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.56.1:5984/db_agenda/_design/agenda/_view/db_agenda";
+    static String url_mto = "http://192.168.56.1:5984/db_agenda/";
+}
